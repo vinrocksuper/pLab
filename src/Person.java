b24108f2e8f7f00e7293028114624c3ae115a2e2
BT_1N3_25@bktech.local <BT_1N3_25@bktech.local>
2018-10-04 09:32:36 -0400

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public abstract class Person {
+    private String firstName;
+    private String familyName;
+
+    public Person(String firstName, String familyName)
+    {
+        this.firstName = firstName;
+        this.familyName = familyName;
+    }
+
+    public abstract String getFirstName();
+    public abstract String getFamilyName();
+    public abstract String toString();
+    public static boolean equals(Person p){
+        return(p.firstName.equals(p.familyName));
+    }
+
+
+}
+
