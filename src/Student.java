60af6ae773e1aed5f47def99272efa64f2350c83
Alston <ashi3454@bths.edu>
2018-10-09 10:10:33 -0400

    asd

--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,27 @@
 public class Student extends Person{
     private double GPA;
+    private double MATHAVG;
+    private double ELAAVG;
 
 public Student(double GPA, String firstName, String familyName){
         super(firstName,familyName);
         this.GPA=GPA;
+        this.MATHAVG=MATHAVG;
+        this.ELAAVG=ELAAVG;
 
 
     }
     public double getGPA(){
         return(this.GPA);
 
+    }
+    public double getMATHAVG()
+    {
+        return(this.MATHAVG);
+    }
+    public double getELAAVG()
+    {
+        return(this.ELAAVG);
     }
     public String getFirstName(){
         return(this.firstName);
